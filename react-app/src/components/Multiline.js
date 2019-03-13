86f2757c4473dfab2fb448d1e39a10af3dba3543
Pradeep Nalabalapu <pnalaba@gmail.com>
2019-03-13 18:14:58 -0500

    code refactoring moving code from render to bind

--- react-app/src/components/Multiline.js
+++ react-app/src/components/Multiline.js
@@ -43,71 +43,110 @@ class Multiline extends Component {
 function makeLineChart(dataset, xName, yObjs, axisLables) {
   var chartObj = {};
   var color = d3.scale.category10();
-  chartObj.xAxisLable = axisLables.xAxis;
-  chartObj.yAxisLable = axisLables.yAxis;
-  /*
-   *      yObjsects format:
-   *           {y1:{column:'',name:'name',color:'color'},y2}
-   *                */
+  //global variables
+  chartObj.rendered = false;
+  var focus = null;
+  var xAxisElem = null;
+  var yAxisElem = null;
+  var legend = null;
+  //grouping tasks that are not data dependent into init
+  function init() {
+    chartObj.xAxisLable = axisLables.xAxis;
+    chartObj.yAxisLable = axisLables.yAxis;
+    /*
+     *      yObjsects format:
+     *           {y1:{column:'',name:'name',color:'color'},y2}
+     *                */
 
-  chartObj.data = dataset;
-  chartObj.margin = { top: 15, right: 60, bottom: 30, left: 50 };
-  chartObj.width = 650 - chartObj.margin.left - chartObj.margin.right;
-  chartObj.height = 480 - chartObj.margin.top - chartObj.margin.bottom;
+    chartObj.data = dataset;
+    chartObj.margin = { top: 15, right: 60, bottom: 30, left: 50 };
+    chartObj.width = 650 - chartObj.margin.left - chartObj.margin.right;
+    chartObj.height = 480 - chartObj.margin.top - chartObj.margin.bottom;
 
-  // So we can pass the x and y as strings when creating the function
-  chartObj.xFunct = function(d) {
-    return d[xName];
-  };
+    // So we can pass the x and y as strings when creating the function
+    chartObj.xFunct = function(d) {
+      return d[xName];
+    };
 
-  // For each yObjs argument, create a yFunction
-  function getYFn(column) {
+    // For each yObjs argument, create a yFunction
+    function getYFn(column) {
+      return function(d) {
+        return d[column];
+      };
+    }
+
+    //// Object instead of array
+    chartObj.yFuncts = [];
+    for (var y in yObjs) {
+      yObjs[y].name = y;
+      yObjs[y].yFunct = getYFn(yObjs[y].column); //Need this  list for the ymax function
+      chartObj.yFuncts.push(yObjs[y].yFunct);
+    }
+
+    ////Formatter functions for the axes
+    chartObj.formatAsNumber = d3.format(".0f");
+    chartObj.formatAsDecimal = d3.format(".2f");
+    chartObj.formatAsCurrency = d3.format("$.2f");
+    chartObj.formatAsFloat = function(d) {
+      if (d % 1 !== 0) {
+        return d3.format(".2f")(d);
+      } else {
+        return d3.format(".0f")(d);
+      }
+    };
+
+    chartObj.xFormatter = chartObj.formatAsNumber;
+    chartObj.yFormatter = chartObj.formatAsFloat;
+
+    chartObj.bisectYear = d3.bisector(chartObj.xFunct).left; //< Can be overridden in definition
+
+    /*
+      // Get the max of every yFunct
+      chartObj.max = function(fn) {
+        return d3.max(chartObj.data, fn);
+      };
+    */
+    chartObj.yScale = d3.scale
+      .linear()
+      .range([chartObj.height, 0])
+      //.domain([0, d3.max(chartObj.yFuncts.map(chartObj.max))]);
+      .domain([0, 1.0]);
+
+    chartObj.formatAsYear = d3.format("");
+
+    chartObj.yAxis = d3.svg
+      .axis()
+      .scale(chartObj.yScale)
+      .orient("left")
+      .tickFormat(chartObj.yFormatter); //< Can be overridden in definition
+
+    // Build line building functions
+  }
+  function getYScaleFn(yObj) {
     return function(d) {
-      return d[column];
+      return chartObj.yScale(yObjs[yObj].yFunct(d));
     };
   }
-
-  //// Object instead of array
-  chartObj.yFuncts = [];
-  for (var y in yObjs) {
-    yObjs[y].name = y;
-    yObjs[y].yFunct = getYFn(yObjs[y].column); //Need this  list for the ymax function
-    chartObj.yFuncts.push(yObjs[y].yFunct);
+  function mouseover_null_fn() {
+    focus.style("display", null);
+  }
+  function mouseout_transition_fn() {
+    focus
+      .transition()
+      .delay(700)
+      .style("display", "none");
+  }
+  function mouseout_none_fn() {
+    focus.style("display", "none");
   }
 
-  ////Formatter functions for the axes
-  chartObj.formatAsNumber = d3.format(".0f");
-  chartObj.formatAsDecimal = d3.format(".2f");
-  chartObj.formatAsCurrency = d3.format("$.2f");
-  chartObj.formatAsFloat = function(d) {
-    if (d % 1 !== 0) {
-      return d3.format(".2f")(d);
-    } else {
-      return d3.format(".0f")(d);
-    }
-  };
-
-  chartObj.xFormatter = chartObj.formatAsNumber;
-  chartObj.yFormatter = chartObj.formatAsFloat;
-
-  chartObj.bisectYear = d3.bisector(chartObj.xFunct).left; //< Can be overridden in definition
+  init();
 
   //Create scale functions
   chartObj.xScale = d3.scale
     .linear()
     .range([0, chartObj.width])
     .domain(d3.extent(chartObj.data, chartObj.xFunct)); //< Can be overridden in definition
-
-  // Get the max of every yFunct
-  chartObj.max = function(fn) {
-    return d3.max(chartObj.data, fn);
-  };
-  chartObj.yScale = d3.scale
-    .linear()
-    .range([chartObj.height, 0])
-    .domain([0, d3.max(chartObj.yFuncts.map(chartObj.max))]);
-
-  chartObj.formatAsYear = d3.format("");
 
   //Create axis
   chartObj.xAxis = d3.svg
@@ -116,18 +155,6 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
     .orient("bottom")
     .tickFormat(chartObj.xFormatter); //< Can be overridden in definition
 
-  chartObj.yAxis = d3.svg
-    .axis()
-    .scale(chartObj.yScale)
-    .orient("left")
-    .tickFormat(chartObj.yFormatter); //< Can be overridden in definition
-
-  // Build line building functions
-  function getYScaleFn(yObj) {
-    return function(d) {
-      return chartObj.yScale(yObjs[yObj].yFunct(d));
-    };
-  }
   for (var yObj in yObjs) {
     yObjs[yObj].line = d3.svg
       .line()
@@ -158,11 +185,16 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
       return false;
     }
 
+    if (!chartObj.rendered) {
+      return false;
+    }
+
     /* Else Update the axis with the new scale */
     chartObj.svg
       .select(".x.axis")
       .attr("transform", "translate(0," + chartObj.height + ")")
       .call(chartObj.xAxis);
+
     chartObj.svg.select(".x.axis .label").attr("x", chartObj.width / 2);
 
     chartObj.svg.select(".y.axis").call(chartObj.yAxis);
@@ -205,14 +237,8 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
       .attr("class", "inner-box");
     var chartSelector = selector + " .inner-box";
     chartObj.chartDiv = d3.select(chartSelector);
-    d3.select(window).on("resize." + chartSelector, chartObj.update_svg_size);
-    chartObj.update_svg_size();
-    return chartObj;
-  };
 
-  // Render the chart
-  chartObj.render = function() {
-    //Create SVG element
+    chartObj.update_svg_size();
     chartObj.svg = chartObj.chartDiv
       .append("svg")
       .attr("class", "chart-area")
@@ -229,44 +255,24 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
         "transform",
         "translate(" + chartObj.margin.left + "," + chartObj.margin.top + ")"
       );
-
-    // Draw Lines
     for (var y in yObjs) {
-      yObjs[y].path = chartObj.svg
-        .append("path")
-        .datum(chartObj.data)
-        .attr("class", "line")
-        .attr("d", yObjs[y].line)
-        .style("stroke", color(y))
-        .attr("data-series", y)
-        .on("mouseover", function() {
-          focus.style("display", null);
-        })
-        .on("mouseout", function() {
-          focus
-            .transition()
-            .delay(700)
-            .style("display", "none");
-        })
-        .on("mousemove", mousemove);
+      yObjs[y].path = chartObj.svg.append("path");
     }
+    //Draw tooltips
+    focus = chartObj.svg
+      .append("g")
+      .attr("class", "focus")
+      .style("display", "none");
 
-    // Draw Axis
-    chartObj.svg
+    // Create elements for Axes
+    xAxisElem = chartObj.svg
       .append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + chartObj.height + ")")
-      .call(chartObj.xAxis)
-      .append("text")
-      .attr("class", "label")
-      .attr("x", chartObj.width / 2)
-      .attr("y", 30)
-      .style("text-anchor", "middle")
-      .text(chartObj.xAxisLable);
+      .attr("transform", "translate(0," + chartObj.height + ")");
 
-    chartObj.svg
-      .append("g")
-      .attr("class", "y axis")
+    yAxisElem = chartObj.svg.append("g").attr("class", "y axis");
+    //assuming here that yaxis doesn't change with time
+    yAxisElem
       .call(chartObj.yAxis)
       .append("text")
       .attr("class", "label")
@@ -276,12 +282,32 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
       .attr("dy", ".71em")
       .style("text-anchor", "middle")
       .text(chartObj.yAxisLable);
+    //
+    // Year label
+    focus
+      .append("text")
+      .attr("class", "focus year")
+      .attr("x", 9)
+      .attr("y", 7);
+    // Focus line
+    focus
+      .append("line")
+      .attr("class", "focus line")
+      .attr("y1", 0)
+      .attr("y2", chartObj.height);
 
-    //Draw tooltips
-    var focus = chartObj.svg
-      .append("g")
-      .attr("class", "focus")
-      .style("display", "none");
+    //Draw legend
+    //Also, assuming here that legend doesn't need to change with time
+    legend = chartObj.mainDiv.append("div").attr("class", "legend");
+    for (y in yObjs) {
+      var series = legend.append("div");
+      series
+        .append("div")
+        .attr("class", "series-marker")
+        .style("background-color", color(y));
+      series.append("p").text(y);
+      yObjs[y].legend = series;
+    }
 
     for (y in yObjs) {
       yObjs[y].tooltip = focus.append("g");
@@ -298,30 +324,37 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
         .attr("dy", ".35em");
     }
 
-    // Year label
-    focus
-      .append("text")
-      .attr("class", "focus year")
-      .attr("x", 9)
-      .attr("y", 7);
-    // Focus line
-    focus
-      .append("line")
-      .attr("class", "focus line")
-      .attr("y1", 0)
-      .attr("y2", chartObj.height);
+    d3.select(window).on("resize." + chartSelector, chartObj.update_svg_size);
 
-    //Draw legend
-    var legend = chartObj.mainDiv.append("div").attr("class", "legend");
-    for (y in yObjs) {
-      var series = legend.append("div");
-      series
-        .append("div")
-        .attr("class", "series-marker")
-        .style("background-color", color(y));
-      series.append("p").text(y);
-      yObjs[y].legend = series;
+    return chartObj;
+  };
+
+  // Render the chart
+  chartObj.render = function() {
+    //Create SVG element
+
+    // Draw Lines
+    for (var y in yObjs) {
+      yObjs[y].path
+        .datum(chartObj.data)
+        .attr("class", "line")
+        .attr("d", yObjs[y].line)
+        .style("stroke", color(y))
+        .attr("data-series", y)
+        .on("mouseover", mouseover_null_fn)
+        .on("mouseout", mouseout_transition_fn)
+        .on("mousemove", mousemove);
     }
+
+    // Draw Axis
+    xAxisElem
+      .call(chartObj.xAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("x", chartObj.width / 2)
+      .attr("y", 30)
+      .style("text-anchor", "middle")
+      .text(chartObj.xAxisLable);
 
     // Overlay to capture hover
     chartObj.svg
@@ -329,13 +362,11 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
       .attr("class", "overlay")
       .attr("width", chartObj.width)
       .attr("height", chartObj.height)
-      .on("mouseover", function() {
-        focus.style("display", null);
-      })
-      .on("mouseout", function() {
-        focus.style("display", "none");
-      })
+      .on("mouseover", mouseover_null_fn)
+      .on("mouseout", mouseout_none_fn)
       .on("mousemove", mousemove);
+
+    chartObj.rendered = true;
 
     return chartObj;
     function mousemove() {
@@ -378,4 +409,5 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
   };
   return chartObj;
 }
+
 export default Multiline;
